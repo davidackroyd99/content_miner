efbabd020d17d90cbf3bae36f2b7edcff4760a38
David Ackroyd <davidackroyd57@gmail.com>
2022-07-28 17:26:44 +0100

    more stuff

--- main.py
+++ main.py
@@ -30,17 +30,28 @@ def parse_sentence(sentence):
     return Sentence(' '.join(sentence.text.split()).strip(), targets, len(targets))
 
 scan_set = []
+scan_mode = False
 found_sentences = []
+target_count = 0
+token_count = 0
 
 for s in doc.sents:
     parsed = parse_sentence(s)
+    target_count += len(parsed.targets)
+    token_count += len(s)
 
     for t in parsed.targets:
         scan_set.append(t)
     if parsed.target_count == 1:
         found_sentences.append(parsed)
 
+print("Percentage of tokens known:", (token_count - target_count) / token_count)
+print()
+
 for t, count in Counter(scan_set).most_common():
+    if scan_mode:
+        print(t)
+        continue
     print(t, count)
 
     for sent in found_sentences:
