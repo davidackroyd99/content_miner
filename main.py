781179f889ebbc45261c7e04897eff27e183a5de
David Ackroyd <davidackroyd57@gmail.com>
2022-08-01 18:16:01 +0100

    made script into proper(ish) console app

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import sys
+
 import analysis
 import file_loader
 from output import print_output
 
-content = file_loader.load_content_file("user/input.txt")
-known_words = file_loader.load_wordlist_file("user/known_words.txt")
-
+content_path = sys.argv[1]
+known_words_path = sys.argv[2]
+options = sys.argv[3:]
 scan_mode = False
+freq = True
+
+if "scan" in options:
+    scan_mode = True
+if "nofreq" in options:
+    freq = False
+
+content = file_loader.load_content_file(content_path)
+known_words = file_loader.load_wordlist_file(known_words_path)
+
 analysed = analysis.analyse_content(content, known_words)
 
-print_output(analysed, scan_mode)
+print_output(analysed, scan_mode, freq)
--- output.py
+++ output.py
@@ -4,19 +4,22 @@ from collections import Counter
 from analysis import AnalysedContent
 
 
-def print_output(analysed_content: AnalysedContent, scan_mode: bool) -> str:
+def print_output(analysed_content: AnalysedContent, scan_mode: bool, freq: bool) -> str:
     """Print the output of content analysis to the console"""
-    print(_produce_output(analysed_content, scan_mode))
+    print(_produce_output(analysed_content, scan_mode, freq))
 
 
-def _produce_output(analysed_content: AnalysedContent, scan_mode: bool) -> str:
+def _produce_output(analysed_content: AnalysedContent, scan_mode: bool, freq: bool) -> str:
     output = [f"Percentage of tokens known: {(analysed_content.token_count - analysed_content.target_count) / analysed_content.token_count}", ""]
 
     for t, count in Counter(analysed_content.all_targets).most_common():
-        if scan_mode:
+        if freq:
+            output.append(f"{t} {count}")
+        else:
             output.append(t)
+
+        if scan_mode:
             continue
-        output.append(f"{t} {count}")
 
         for sent in analysed_content.sentences:
             if sent.target_count > 0 and sent.targets[0] == t:
