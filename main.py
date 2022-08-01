05f0434f138f81df83094f65df7361d982761f4f
David Ackroyd <davidackroyd57@gmail.com>
2022-08-01 17:32:55 +0100

    analysis module

--- main.py
+++ main.py
@@ -1,44 +1,26 @@
-import spacy
 
 from collections import Counter
 
+import analysis
 import file_loader
-import sentence
 
-nlp = spacy.load("es_core_news_sm")
-
-text = file_loader.load_content_file("user/input.txt")
+content = file_loader.load_content_file("user/input.txt")
 known_words = file_loader.load_wordlist_file("user/known_words.txt")
 
-doc = nlp(text)
+scan_mode = False
+analysed = analysis.analyse_content(content, known_words)
 
-scan_set = []
-scan_mode = True
-found_sentences = []
-target_count = 0
-token_count = 0
-
-for s in doc.sents:
-    parsed = sentence.analyse_sentence(s, known_words)
-    target_count += len(parsed.targets)
-    token_count += len(s)
-
-    for t in parsed.targets:
-        scan_set.append(t)
-    if parsed.target_count == 1 or parsed.target_count == 2:
-        found_sentences.append(parsed)
-
-print("Percentage of tokens known:", (token_count - target_count) / token_count)
+print("Percentage of tokens known:", (analysed.token_count - analysed.target_count) / analysed.token_count)
 print()
 
-for t, count in Counter(scan_set).most_common():
+for t, count in Counter(analysed.all_targets).most_common():
     if scan_mode:
         print(t)
         continue
     print(t, count)
 
-    for sent in found_sentences:
-        if sent.targets[0] == t:
+    for sent in analysed.sentences:
+        if sent.target_count > 0 and sent.targets[0] == t:
             try:
                 print(sent.text)
             except:
