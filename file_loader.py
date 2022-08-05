9305b297492a052291f8bd22d7cfd032dff1b92b
David Ackroyd <davidackroyd57@gmail.com>
2022-08-05 15:48:17 +0100

    4: duplicates are removed from wordlist on file load

--- file_loader.py
+++ file_loader.py
@@ -10,10 +10,14 @@ def load_content_file(fp: str) -> str:
 
 def load_wordlist_file(fp: str) -> Collection[str]:
     """Get list of known words as a collection of strings"""
-    known_words = []
+    known_words = set()
 
     with open(fp, encoding='utf-8') as known_file:
         for line in known_file:
-            known_words.append(line.strip())
+            known_words.add(line.strip())
+
+    with open(fp, mode='w', encoding='utf-8') as known_file:
+        known_file.write("\n".join(sorted(known_words)))
+
 
     return known_words
