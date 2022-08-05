cf92abdd8d940e0cbe6062725ddbcdbdcf08ff2b
David Ackroyd <davidackroyd57@gmail.com>
2022-08-05 16:10:40 +0100

    23: handles srt files

--- file_loader.py
+++ file_loader.py
@@ -1,11 +1,24 @@
 """Load files for analysis"""
 from typing import Collection
+import srt
+import unittest
+
+
+def _remove_srt(content: str) -> str:
+    subs = srt.parse(content)
+    return " ".join([s.content for s in subs])
 
 
 def load_content_file(fp: str) -> str:
     """Get contents of content file as a string"""
+    content = ""
     with open(fp, encoding='utf-8') as input_file:
-        return input_file.read()
+        content = input_file.read()
+
+    if fp[-4:] == ".srt":
+        content = _remove_srt(content)
+
+    return content
 
 
 def load_wordlist_file(fp: str) -> Collection[str]:
@@ -21,3 +34,28 @@ def load_wordlist_file(fp: str) -> Collection[str]:
 
 
     return known_words
+
+
+class RemoveSrtTestCase(unittest.TestCase):
+    def test_srt(self):
+        srt = '''\
+1
+00:31:37,894 --> 00:31:39,928
+OK, look, I think I have a plan here.
+
+2
+00:31:39,931 --> 00:31:41,931
+Using mainly spoons,
+
+3
+00:31:41,933 --> 00:31:43,435
+we dig a tunnel under the city and release it into the wild.
+
+'''
+        srt_removed = "OK, look, I think I have a plan here. Using mainly spoons, we dig a tunnel under the city and " \
+                      "release it into the wild."
+
+        self.assertEqual(_remove_srt(srt), srt_removed)
+
+if __name__ == '__main__':
+    unittest.main()
